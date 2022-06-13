64ede5e4ba356d859c0ca8d85801a48d534344ec
Jinseok Seo (Jason Jin) <jinseok9338@gmail.com>
2022-06-13 14:27:26 +0000

    Simple hello world with yew

--- os_with_rust/src/main.rs
+++ os_with_rust/src/main.rs
@@ -1,3 +1,10 @@
+use yew::{function_component, html};
+
+#[function_component(HelloWorld)]
+fn hello_world() -> Html {
+    html! { "Hello world" }
+}
+
 fn main() {
-    println!("Hello, world!");
-} 
+    yew::start_app::<HelloWorld>();
+}
