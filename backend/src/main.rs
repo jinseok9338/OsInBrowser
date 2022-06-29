eb5790d27fc1f8afb99b89ce2f67cd19aa720a75
jinseok9338 <jinseok9338@gmail.com>
2022-06-29 10:29:55 +0000

    making backend for serving the spa

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer};
+use actix_web_lab::web::spa;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(move || {
+        App::new().service(
+            spa()
+                .index_file("./dist/index.html")
+                .static_resources_mount("./")
+                .static_resources_location("./dist")
+                .finish(),
+        )
+    })
+    .bind(("0.0.0.0", 3000))?
+    .run()
+    .await
 }
