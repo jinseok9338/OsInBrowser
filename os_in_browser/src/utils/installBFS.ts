79b678973dbc7f00c897dbf60eabbf304420773c
jinseok9338 <jinseok9338@gmail.com>
2022-07-10 07:29:25 +0000

    remaking the localstorage

--- os_in_browser/src/utils/installBFS.ts
+++ os_in_browser/src/utils/installBFS.ts
@@ -2,12 +2,7 @@ import BrowserFS from "browserfs";
 
 const BFSOption = {
   "/recents": { fs: "InMemory" },
-  "/home/apps": { fs: "LocalStorage" },
-  "/home/folder": { fs: "LocalStorage" },
-  "/home/desktop": { fs: "LocalStorage" },
-  "/home/documents": { fs: "LocalStorage" },
-  "/home/downlaods": { fs: "LocalStorage" },
-  "/home/pictures": { fs: "LocalStorage" },
+  "/home": { fs: "LocalStorage" },
 };
 
 export const installBFS = () => {
