fc07570a188a167b5c1c1569d673fd7c7df8ea44
Jinseok Seo (Jason Jin) <jinseok9338@gmail.com>
2022-09-03 19:30:06 +0900

    Update vite.config.ts

--- my-solid-project/vite.config.ts
+++ my-solid-project/vite.config.ts
@@ -5,16 +5,11 @@ export default defineConfig(({ command, mode, ssrBuild }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
-    define: {
-      WEATHER_SECRET: JSON.stringify(env.WEATHER_SECRET),
-      global: {},
-    },
+
     plugins: [solidPlugin()],
     server: {
       port: 3000,
     },
-    build: {
-      target: "esnext",
-    },
+
   };
 });
